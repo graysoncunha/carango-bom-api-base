024e2248a0d9bc1282c1c7fba6beb300564218e8
Grayson Cunha <grayson.cunha@thoughtworks.com>
2021-08-24 16:58:58 -0300

    fix: remove o path comum nos endpoints e faz uso do RequestMapping

--- src/main/java/br/com/caelum/carangobom/marca/MarcaController.java
+++ src/main/java/br/com/caelum/carangobom/marca/MarcaController.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/marcas")
 public class MarcaController {
 
     private MarcaRepository mr;
@@ -26,13 +27,13 @@ public class MarcaController {
         this.mr = mr;
     }
 
-    @GetMapping("/marcas")
+    @GetMapping
     @Transactional
     public List<Marca> lista() {
         return mr.findAllByOrderByNome();
     }
 
-    @GetMapping("/marcas/{id}")
+    @GetMapping("/{id}")
     @Transactional
     public ResponseEntity<Marca> id(@PathVariable Long id) {
         Optional<Marca> m1 = mr.findById(id);
@@ -43,7 +44,7 @@ public class MarcaController {
         }
     }
 
-    @PostMapping("/marcas")
+    @PostMapping
     @Transactional
     public ResponseEntity<Marca> cadastra(@Valid @RequestBody Marca m1, UriComponentsBuilder uriBuilder) {
         Marca m2 = mr.save(m1);
@@ -51,7 +52,7 @@ public class MarcaController {
         return ResponseEntity.created(h).body(m2);
     }
 
-    @PutMapping("/marcas/{id}")
+    @PutMapping("/{id}")
     @Transactional
     public ResponseEntity<Marca> altera(@PathVariable Long id, @Valid @RequestBody Marca m1) {
         Optional<Marca> m2 = mr.findById(id);
@@ -64,8 +65,7 @@ public class MarcaController {
         }
     }
 
-    @DeleteMapping("/marcas/{id}")
-
+    @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity<Marca> deleta(@PathVariable Long id) {
         Optional<Marca> m1 = mr.findById(id);
