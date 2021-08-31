643dd2a545835f7f43e4506d419d0d552916c06a
Bruno Bacelar <knuxbbs@gmail.com>
2021-08-31 07:54:25 -0300

    Remove DomainException

--- src/main/java/br/com/caelum/carangobom/DomainException.java
+++
@@ -1,9 +0,0 @@
-package br.com.caelum.carangobom;
-
-public class DomainException extends RuntimeException {
-  private static final long serialVersionUID = 1050812768976980295L;
-
-  public DomainException(String message) {
-    super(message);
-  }
-}
